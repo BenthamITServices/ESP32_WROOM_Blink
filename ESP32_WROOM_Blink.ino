c9311b97acd678ed82ae9080b7ac9d7ac1b97486
Steven Bennett <2668285+BenthamITServices@users.noreply.github.com>
2023-04-05 15:48:24 +0100

    Update ESP32_WROOM_Blink.ino

--- ESP32_WROOM_Blink.ino
+++ ESP32_WROOM_Blink.ino
@@ -1,16 +1,16 @@
 /*
   Blink Built-in LED on Digital Pin 2
 */
-#define LED_BUILTIN 2
+#define LED 2
 
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(LED_Yellow, OUTPUT);
 }
 
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
+  digitalWrite(LED, HIGH);  // turn the LED on (HIGH is the voltage level)
   delay(1000);                      // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);   // turn the LED off by making the voltage LOW
+  digitalWrite(LED, LOW);   // turn the LED off by making the voltage LOW
   delay(100);                      // wait for a second
 }
